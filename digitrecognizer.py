78cba7930918be2a88080d59b21b9492be5b219a
Trickshotblaster <107130695+Trickshotblaster@users.noreply.github.com>
2023-05-04 17:32:04 -0700

    Update digitrecognizer.py

--- digitrecognizer.py
+++ digitrecognizer.py
@@ -1,29 +1,26 @@
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import json
 
-activation_dict = {
-    'sigmoid': lambda x: 1 / (np.exp(-x)),
-    'tanh': np.tanh,
-    'relu': lambda x: np.maximum(0, x)
-}
 
-derivative_dict = {
-    'sigmoid': lambda x: x * (1 - x),
-    'tanh': lambda x: 1 - np.tanh(x) ** 2,
-    'relu': lambda x: np.where(x > 0, 1, 0)
-}
+def find_highest(arr):
+    highest = 0
+    index = 0
+    for i, v in enumerate(arr):
+        if v > highest:
+            highest = v
+            index = i
+    return [index, round(highest, 3)]
 
 
 class CustomNeuralNetwork:
-    def __init__(self, layer_sizes, weights=[], biases=[], activation_function='sigmoid',
-                 save_file="nn_weights_biases.txt"):
+    def __init__(self, layer_sizes, weights=[], biases=[], save_file="nn_weights_biases.txt"):
         self.layer_sizes = layer_sizes
         self.weights = []
         self.biases = []
         self.activations = []
         self.save_file = save_file
-        self.activation_function = activation_dict[activation_function]
-        self.derivative_function = derivative_dict[activation_function]
         if weights == []:
             # Initialize weights and biases
             for i in range(len(layer_sizes) - 1):
@@ -57,14 +54,15 @@ class CustomNeuralNetwork:
         data = json.loads(serialized_data)
         weights = [np.array(w) for w in data["weights"]]
         biases = [np.array(b) for b in data["biases"]]
+        layer_sizes = [w.shape[0] for w in weights] + [weights[-1].shape[1]]
         self.weights = weights
         self.biases = biases
 
-    def activation(self, x):
-        return self.activation_function(x)
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def activation_derivative(self, x):
-        return self.derivative_function(x)
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
 
     def forward(self, X):
         layer_input = X
@@ -72,8 +70,7 @@ class CustomNeuralNetwork:
 
         for i in range(len(self.layer_sizes) - 1):
             layer_output = np.dot(layer_input, self.weights[i]) + self.biases[i]
-
-            layer_input = self.activation(layer_output)
+            layer_input = self.sigmoid(layer_output)
             self.activations.append(layer_input)
 
         return layer_input
@@ -83,14 +80,15 @@ class CustomNeuralNetwork:
         deltas = []
 
         # Calculate output layer error
-        error = (output - y) ** 2
-        delta = error * self.activation_derivative(output)
+        error = output - y
+        print(np.average(error))
+        delta = error * self.sigmoid_derivative(output)
         deltas.append(delta)
 
         # Calculate hidden layer errors
         for i in reversed(range(len(self.weights) - 1)):
             error = np.dot(delta, self.weights[i + 1].T)
-            delta = error * self.activation_derivative(self.activations[i])
+            delta = error * self.sigmoid_derivative(self.activations[i])
             deltas.append(delta)
 
         # Reverse deltas to match layer order
@@ -104,7 +102,7 @@ class CustomNeuralNetwork:
 
     def train(self, X_train, y_train, batch_size, learning_rate, epochs):
         for epoch in range(epochs):
-            self.save_to_file(self.save_file)
+            self.save_to_file("nn_weights_biases.txt")
             # Shuffle the dataset
             indices = np.arange(X_train.shape[0])
             np.random.shuffle(indices)
@@ -120,27 +118,61 @@ class CustomNeuralNetwork:
                 self.backpropagate(X_batch, y_batch, learning_rate)
 
 
-
 # Training parameters
 learning_rate = 0.01
 epochs = 10000
 
-(x_train, y_train), (x_test, y_test) = (np.array([[1], [3], [5], [7]]), np.array([[0], [0], [0], [0]])), (
-np.array([[2], [4], [6], [8]]), np.array([[0], [0], [0], [0]]))
+# Load the MNIST dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the pixel values
+x_train = x_train / 255.0
+x_test = x_test / 255.0
 
 # Reshape the input data
 x_train = x_train.reshape(x_train.shape[0], -1)
 x_test = x_test.reshape(x_test.shape[0], -1)
 
+# Convert the labels to one-hot encoding
+y_train = tf.keras.utils.to_categorical(y_train)
+y_test = tf.keras.utils.to_categorical(y_test)
+
 # Create and train a neural network
-layer_sizes = [1, 1]
+layer_sizes = [784, 128, 100, 10]
 nn = CustomNeuralNetwork(layer_sizes)
-# if neural net loading doesn't work, remove this (you need a text file in your project to store the weights and biases)
+# if neural net loading doesn't work, remove this
 try:
-    nn.load_from_file("nn_weights_biases.txt")  # rename to whatever your text file is
+    nn.load_from_file("nn_weights_biases.txt")
     nn.forward(x_test)
 except:
     print("network could not be read from file")
+# -+-nn.train(x_train, y_train, batch_size=10, learning_rate=0.05, epochs=25)
 
-nn.train(x_train, y_train, batch_size=10, learning_rate=0.01, epochs=100)
+
+# Test the trained network
+y_pred = nn.forward(x_test)
+y_true = np.argmax(y_test, axis=1)
+accuracy = np.mean(y_true == y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+
+# Function to display the images and their labels
+def display_images(images, true_labels, pred_labels, cmap=plt.cm.binary):
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i], cmap=cmap)
+        plt.xlabel(f"True: {true_labels[i]}, Pred: {find_highest(pred_labels[i])}")
+    plt.show()
+
+
+# Reshape the test images back to their original format
+x_test_reshaped = x_test.reshape(x_test.shape[0], 28, 28)
+
+# Display the images along with their true and predicted labels
+display_images(x_test_reshaped, y_true, y_pred)
 
